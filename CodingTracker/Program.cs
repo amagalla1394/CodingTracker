5f7289458f106569138d764281851154152a600e
amagalla <angelmagallanes18@gmail.com>
2023-07-30 21:44:01 -0500

    Initial Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more modification!");
+﻿void MyFirstMethod()
+{
+
+}
+
+void MySecondMethod()
+{
+
+}
+
+void MyThirdMethod()
+{
+
+}
